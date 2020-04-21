559d2f10ffa5ad763ca0d6d25641f0a22c326746
Allef Gomes <allefgalmeida@gmail.com>
2020-04-21 01:20:40 -0300

    Fix sign_up function to create user

--- lib/bank_api_web/controllers/users_controller.ex
+++ lib/bank_api_web/controllers/users_controller.ex
@@ -1,9 +1,11 @@
 defmodule BankApiWeb.UserController do
   use BankApiWeb, :controller
+  alias BankApi.Accounts
 
   def create(conn, %{"user" => user}) do
+    {:ok, account} = Accounts.create_user(user)
     conn
     |> put_status(201)
-    |> render("user.json", %{"user" => user})
+    |> render("account.json", %{account: account})
   end
 end
