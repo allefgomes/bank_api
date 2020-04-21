d891b7b132b93a187ae46ca9260e5171a246e408
Allef Gomes <allefgalmeida@gmail.com>
2020-04-21 15:51:40 -0300

    Create view & controller to changeset errors

---
+++ lib/bank_api_web/views/changeset_view.ex
@@ -0,0 +1,11 @@
+defmodule BankApiWeb.ChangesetView do
+  use BankApiWeb, :view
+
+  def render("error.json", %{changeset: changeset}) do
+    %{ errors: translate_errors(changeset) }
+  end
+
+  defp translate_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, &translate_error/1)
+  end
+end
