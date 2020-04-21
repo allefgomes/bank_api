e4ea196310ae617e0d887bcd9ff7a9efb7f848a6
Allef Gomes <allefgalmeida@gmail.com>
2020-04-20 23:23:59 -0300

    Config credo

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule BankApi.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
