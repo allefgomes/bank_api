437843999f435b180cedd256e8bcc77c03afe6e0
Allef Gomes <allefgalmeida@gmail.com>
2020-04-22 23:05:34 -0300

    Refactor operations

--- lib/bank_api/operations.ex
+++ lib/bank_api/operations.ex
@@ -2,7 +2,11 @@ defmodule BankApi.Operations do
   alias BankApi.Accounts
   alias BankApi.Accounts.Account
   alias BankApi.Repo
+  alias BankApi.Transactions.Transaction
+  alias Ecto.Multi
 
+  @withdraw "withdraw"
+  @transfer "transfer"
   def transfer(from_id, to_id, value) do
     from = Accounts.get!(from_id)
     value = Decimal.new(value)
@@ -22,29 +26,52 @@ defmodule BankApi.Operations do
         {:error, "you can`t have negative balance!"}
 
       false ->
-        {:ok, from} = perform_operation(from, value, :sub) |> Repo.update()
-        {:ok, "Withdraw with success!! from: #{user_name(from)}, value: #{value}"}
+        withdraw_operations(from, value)
     end
   end
 
-  defp is_negative?(from_balance, value) do
-    Decimal.sub(from_balance, value)
-    |> Decimal.negative?()
+  defp withdraw_operations(from, value) do
+    message = "Withdraw with success!! from: #{user_name(from)}, value: #{value}"
+
+    Multi.new()
+    |> Multi.update(:account_from, perform_operation(from, value, :sub))
+    |> Multi.insert(:transaction, generate_transaction(from.id, nil, @withdraw, value))
+    |> Repo.transaction()
+    |> transaction_case(message)
   end
 
   def perform_update(from, to_id, value) do
     to = Accounts.get!(to_id)
 
-    transaction =
-      Ecto.Multi.new()
-      |> Ecto.Multi.update(:account_from, perform_operation(from, value, :sub))
-      |> Ecto.Multi.update(:account_to, perform_operation(to, value, :add))
-      |> Repo.transaction()
+    message =
+      "Tranfer with success!! from: #{user_name(from)} to: #{user_name(to)} value: #{value}"
 
-    case transaction do
+    Multi.new()
+    |> Multi.update(:account_from, perform_operation(from, value, :sub))
+    |> Multi.update(:account_to, perform_operation(to, value, :add))
+    |> Multi.insert(:transaction, generate_transaction(from.id, to.id, @transfer, value))
+    |> Repo.transaction()
+    |> transaction_case(message)
+  end
+
+  def perform_operation(account, value, :sub) do
+    account
+    |> update_account(%{balance: Decimal.sub(account.balance, value)})
+  end
+
+  def perform_operation(account, value, :add) do
+    account
+    |> update_account(%{balance: Decimal.add(account.balance, value)})
+  end
+
+  defp update_account(%Account{} = account, attrs) do
+    Account.changeset(account, attrs)
+  end
+
+  defp transaction_case(operations, message) do
+    case operations do
       {:ok, _} ->
-        {:ok,
-         "Tranfer with success!! from: #{user_name(from)} to: #{user_name(to)} value: #{value}"}
+        {:ok, message}
 
       {:error, :account_from, changeset, _} ->
         {:error, changeset}
@@ -58,17 +85,18 @@ defmodule BankApi.Operations do
     account.user.name
   end
 
-  def perform_operation(account, value, :sub) do
-    account
-    |> update_account(%{balance: Decimal.sub(account.balance, value)})
+  defp generate_transaction(from_id, to_id, type, value) do
+    %Transaction{
+      account_from: from_id,
+      account_to: to_id,
+      type: type,
+      value: value,
+      date: Date.utc_today()
+    }
   end
 
-  def perform_operation(account, value, :add) do
-    account
-    |> update_account(%{balance: Decimal.add(account.balance, value)})
-  end
-
-  def update_account(%Account{} = account, attrs) do
-    Account.changeset(account, attrs)
+  defp is_negative?(balance, value) do
+    Decimal.sub(balance, value)
+    |> Decimal.negative?()
   end
 end
